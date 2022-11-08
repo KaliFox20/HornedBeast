6caf8c06edb09d49f6c62b7eb60d1f59802ef3d9
KaliFox20 <Daniel.Shiraishi@gmail.com>
2022-11-08 13:11:22 -1000

    changes to lab1

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,11 +3,19 @@ import React from 'react';
 class HornedBeast extends React.Component {
 
   render() {
+    /*
+   "_id": 1,
+    "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+    "title": "UniWhal",
+    "description": "A unicorn and a narwhal nuzzling their horns",
+    "keyword": "narwhal",
+    "horns": 1
+    */
 return(
   <>
-  <h2>Insert Title Here</h2>
-  <img src='https://place-hold.it/300x375/ddd'alt=''title=''></img>
-  <p>Insert Something Witty Here</p>
+  <h2>{this.props.title}</h2>
+  <img src={this.props.imageURL}alt={this.props.description}title={this.props.title}></img>
+  <p>{this.props.description}</p>
   </>
 );
   }
