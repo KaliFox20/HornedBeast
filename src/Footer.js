847008100120bb0041745fa5c715bd89d724ae8b
KaliFox20 <Daniel.Shiraishi@gmail.com>
2022-11-08 00:36:15 -1000

    Lab1 301

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render() {
+return(
+  <h2>Daniel Shiraishi</h2>
+);
+  }
+}
+
+export default Footer;
---
+++ src/HornedBeast.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+
+  render() {
+return(
+  <>
+  <h2>Insert Title Here</h2>
+  <img src='https://place-hold.it/300x375/ddd'alt=''title=''></img>
+  <p>Insert Something Witty Here</p>
+  </>
+);
+  }
+}
+
+export default HornedBeast;
