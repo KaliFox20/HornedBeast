ae162e85e02e52ac827dcc27d85f774da5a0317c
KaliFox20 <Daniel.Shiraishi@gmail.com>
2022-11-12 11:15:24 -1000

    Fixed version of Lab2

--- src/Main.js
+++ src/Main.js
@@ -6,23 +6,30 @@ class Main extends React.Component {
 
   render() {
 
-    let beastArr = this.props.data.map((beast, idx) => {
-      return <HornedBeast
-        id={beast._id}
-        title={beast.title}
-        image_url={beast.image_url}
-        description={beast.description}
-        horns={beast.horns}
-        keyword={beast.keyword}
-        key={idx}
-      />
 
-    });
+
+
     return (
       <main>
-        {beastArr}
+        <>
+          {
+            data.map((beast) => (
+
+              <HornedBeast
+                _id={beast._id}
+                title={beast.title}
+                image_url={beast.image_url}
+                horns={beast.horns}
+                keyword={beast.keyword}
+                description={beast.description}
+              />
+            ))
+          }
+
+        </>
+
       </main >
-    );
+    )
 
 
   }
