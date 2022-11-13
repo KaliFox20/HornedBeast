33fe08116d5d4bff2b6de003ac72f82990db8cf4
KaliFox20 <Daniel.Shiraishi@gmail.com>
2022-11-12 14:15:28 -1000

    updated Lab3

--- src/Main.js
+++ src/Main.js
@@ -5,18 +5,33 @@ import HornedBeast from './HornedBeast';
 class Main extends React.Component {
 
   render() {
-    let beastArr = []; 
-    data.map(v, i);
-    
-  }
-  return(
-      <main>
-        <HornedBeast />
+    return (
+      < main >
+      <>
+        {
+          data.map((beast) => (
 
-        <HornedBeast />
-      </main >
-    );
+            <HornedBeast
+              _id={beast._id}
+              title={beast.title}
+              image_url={beast.image_url}
+              horns={beast.horns}
+              keyword={beast.keyword}
+              description={beast.description}
+            />
+          ))
+        }
+
+      </>
+
+    </main >
+    )
+
+
   }
+
+
+
 }
 
 export default Main;
